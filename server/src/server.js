7052c3e870cb99d01e5a98cf3afea08281693168
Emmanuel obonyo <emmyobonyo@gmail.com>
2023-10-31 12:21:20 +0300

    Create scheme for launches

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
@@ -7,7 +8,18 @@ const server = http.createServer(app)
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://komagumobonyo:G89uxVxOPI6Ag3u2@cluster0.p9gn28p.mongodb.net/';
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready');
+})
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
