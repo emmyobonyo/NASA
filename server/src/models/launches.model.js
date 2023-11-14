b703968f4e222d3ba56dbf08175824a195d2b9f6
Emmanuel obonyo <emmyobonyo@gmail.com>
2023-11-14 09:05:33 +0300

    pagination is working

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -105,9 +105,11 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
+async function getAllLaunches(skip, limit) {
   return await launchesDatabase
-    .find({}, { '_id': 0, '__v': 0 });
+    .find({}, { '_id': 0, '__v': 0 })
+    .skip(skip)
+    .limit(limit);
 }
 
 
